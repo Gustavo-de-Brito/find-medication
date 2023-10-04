5bc4239dfbcaeaee1dd63ac97084066fa4f34840
Gustavo-de-Brito <gustavoaraujo086@gmail.com>
2023-10-04 11:00:57 -0300

    feat: add list medication layout

---
+++ src/components/MedicineItem/index.jsx
@@ -0,0 +1,24 @@
+import P from 'prop-types';
+
+const MedicineItem = ({ name, company }) => {
+  return (
+    <a
+      href="#"
+      className="list-group-item list-group-item-action bg-primary
+        bg-opacity-75 text-white"
+      aria-current="true"
+    >
+      <div className="d-flex w-100 justify-content-between">
+        <h3 className="my-3 fs-5 fw-bold">{name}</h3>
+      </div>
+      <p>{company}</p>
+    </a>
+  );
+};
+
+MedicineItem.propTypes = {
+  name: P.string,
+  company: P.string,
+};
+
+export default MedicineItem;
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,11 +1,20 @@
 import './home.css';
 import SearchMedication from '../../components/SearchMedication';
+import { useState } from 'react';
+import MedicineList from '../../components/MedicineList';
 
 const Home = () => {
+  const [medicines] = useState([]);
+
   return (
     <div className="view-container bg-primary bg-opacity-75">
       <div className="content mx-auto p-4 shadow-lg rounded-4 bg-white">
         <SearchMedication />
+        <h3 className="text-primary ms-4 mt-4">Medicamentos</h3>
+        <MedicineList
+          foundedMedicine={medicines.length > 0}
+          medicines={medicines}
+        />
       </div>
     </div>
   );
